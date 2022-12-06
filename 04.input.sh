84b6a730d502be7fc0f8b226249e27045f968c92
Sudheer4reddy <reddigarisudheer447@gmail.com>
2022-12-06 18:11:44 +0530

    Input Special Variables

--- 04.input.sh
+++ 04.input.sh
@@ -2,3 +2,12 @@
 
 read -p "Enter your name : " name
 echo "Your name = $name"
+
+#Special Variable
+
+#$0-$n , $* / $@, $#
+
+echo Script Name = $0
+echo First argument = $1
+echo All Arguments = $*
+echo Number of arguments = $#
