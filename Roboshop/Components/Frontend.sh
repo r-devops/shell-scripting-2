3f9b8a89773dd81664c74dba30ab0a6fcdd36243
Sudheer4reddy <reddigarisudheer447@gmail.com>
2022-12-17 15:25:48 +0530

    Making Frontend component

--- Roboshop/Components/Frontend.sh
+++ Roboshop/Components/Frontend.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
 
+USER_ID=$(id -u)
+if [ "$USER_ID" -ne 0 ]; then
+  echo You should run your script as sudo or root user
+  exit 1
+fi
+
+
 echo -e "\e[36m Installing Nginx \e[0m"
 yum install nginx -y
+if [ $? -eq 0 ] ; then
+  echo -e "\e[32mSUCCESS\e[0m"
+else
+  echo "\e[31mFAILURE\e[0m"
+  exit 1
+fi
 
 echo -e "\e[36m Downloading Nginx Content \e[0m"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+if [ $? -eq 0 ] ; then
+  echo -e "\e[32mSUCCESS\e[0m"
+else
+  echo "\e[31mFAILURE\e[0m"
+  exit 1
+fi
 
 echo -e "\e[36m Cleanup old Nginx content and Extract new downloaded archive \e[0m"
 rm -rf /usr/share/nginx/html
@@ -14,6 +33,12 @@ mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
+if [ $? -eq 0 ] ; then
+  echo -e "\e[32mSUCCESS\e[0m"
+else
+  echo "\e[31mFAILURE\e[0m"
+  exit 1
+fi
 
 echo -e "\e[36m Starting Nginx \e[0m"
 systemctl restart nginx
