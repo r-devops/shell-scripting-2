f8c3e0d813bb923def36f2e5a48ab7116f52ceef
Sudheer4reddy <reddigarisudheer447@gmail.com>
2022-12-20 07:49:46 +0530

    Making Frontend component

--- Roboshop/Components/Frontend.sh
+++ Roboshop/Components/Frontend.sh
@@ -17,28 +17,29 @@ if [ "$USER_ID" -ne 0 ]; then
   echo You should run your script as sudo or root user
   exit 1
 fi
-
+LOG_FILE=temp/Roboshop.log
+rm -rf $LOG_FILE
 
 print "Installing Nginx"
-yum install nginx -y
+yum install nginx -y >>$LOG_FILE
 StatCheck $?
 
 print "Downloading Nginx Content"
-curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" >>$LOG_FILE
 StatCheck $?
 
 print "Cleanup old Nginx content"
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* >>$LOG_FILE
 StatCheck $?
 
 cd /usr/share/nginx/html/
 
 print "Extract new downloaded archive"
-unzip /tmp/frontend.zip && mv frontend-main/* . && mv static/* .
+unzip /tmp/frontend.zip >>$LOG_FILE && mv frontend-main/* . >>$LOG_FILE && mv static/* . >>$LOG_FILE
 StatCheck $?
 
 print "Updating RoboShop Configuration"
-mv localhost.conf /etc/nginx/default.d/roboshop.conf
+mv localhost.conf /etc/nginx/default.d/roboshop.conf >>$LOG_FILE
 StatCheck $?
 
 print "Starting Nginx"
