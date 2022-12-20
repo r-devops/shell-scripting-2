1f000d54b751149ee2276ac051f484c3bb3b79ae
Sudheer4reddy <reddigarisudheer447@gmail.com>
2022-12-20 16:49:13 +0530

    Making Catalogue component

---
+++ roboshop/components/catalogue.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+source components/common.sh
+
+print "Configure YUM Repos"
+curl -fsSL https://rpm.nodesource.com/setup_16.x | sudo bash - &>>${LOG_FILE}
+StatCheck $?
+
+print "Install NodeJS"
+yum install nodejs gcc-c++ -y &>>${LOG_FILE}
+StatCheck $?
+
+print "Add Application User"
+id ${APP_USER} &>>${LOG_FILE}
+if [ $? -ne 0 ]; then
+  useradd ${APP_USER} &>>${LOG_FILE}
+fi
+StatCheck $?
+
+print "Download App Component"
+curl -f -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>${LOG_FILE}
+StatCheck $?
+
+print "CleanUp OLd Content"
+rm -rf /home/${APP_USER}/catalogue &>>${LOG_FILE}
+StatCheck $?
+
+print "Extract App Content"
+cd /home/${APP_USER} &>>${LOG_FILE} && unzip /tmp/catalogue.zip &>>${LOG_FILE} && mv catalogue-main catalogue &>>${LOG_FILE}
+StatCheck $?
+
+print "Install App Dependencies"
+cd /home/${APP_USER}/catalogue &>>${LOG_FILE} && npm install &>>${LOG_FILE}
+StatCheck $?
