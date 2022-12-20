bfa2df30692bbd6148674e3a717b3c6dbd183bf1
Sudheer4reddy <reddigarisudheer447@gmail.com>
2022-12-20 10:47:14 +0530

    Making Frontend component

--- Roboshop/Components/Frontend.sh
+++ Roboshop/Components/Frontend.sh
@@ -18,7 +18,7 @@ if [ "$USER_ID" -ne 0 ]; then
   echo You should run your script as sudo or root user
   exit 1
 fi
-LOG_FILE=tmp/roboshop.log
+LOG_FILE=tmp/Roboshop.log
 rm -rf $LOG_FILE
 
 print "Installing Nginx"
