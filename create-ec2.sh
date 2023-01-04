ac3ec9c09e05ee86410791b0e620a3524a51e971
Sudheer4reddy <reddigarisudheer447@gmail.com>
2023-01-04 12:32:38 +0530

    creating ec2 instance

--- create-ec2.sh
+++ create-ec2.sh
@@ -13,10 +13,10 @@ create_ec2() {
  PRIVATE_IP=$(aws ec2 run-instances \
   --image-id ${AMI_ID} \
   --instance-type t2.micro \
-  --tag-specifications "ResourceType=instance, Tags=[{Key=Name, Value=${COMPONENT}}]" \
-  --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType=persistent, InstanceInterruptionBehavior=stop}" \
+  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" \
+  --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" \
   --security-group-ids ${SGID} \
-  | jq '.Instances []. PrivateIpAddress' | sed -e 's/"//g')
+  | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
   sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}/" route53.json >/tmp/record.json
   aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json | jq
